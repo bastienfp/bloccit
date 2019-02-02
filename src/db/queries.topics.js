bdf01dd16a582b436994d6b10036b4ba592109aa
bastienfp <bastien.fp@gmail.com>
2019-02-02 12:40:48 -0500

    Completed checkpoint The Topic Resource Part: 2

--- src/db/queries.topics.js
+++ src/db/queries.topics.js
@@ -3,13 +3,66 @@ const Topic = require("./models").Topic;
 module.exports = {
 
   getAllTopics(callback){
-    return Topic.all()
-
+    return Topic.findAll()
     .then((topics) => {
       callback(null, topics);
     })
     .catch((err) => {
       callback(err);
+    });
+  },
+
+  getTopic(id, callback){
+    return Topic.findByPk(id)
+    .then((topic) => {
+      callback(null, topic);
     })
+    .catch((err) => {
+      callback(err);
+    });
+  },
+
+  addTopic(newTopic, callback){
+    return Topic.create({
+      title: newTopic.title,
+      description: newTopic.description
+    })
+    .then((topic) => {
+      callback(null, topic);
+    })
+    .catch((err) => {
+      callback(err);
+    });
+  },
+
+  deleteTopic(id, callback){
+    return Topic.destroy({
+      where: {id}
+    })
+    .then((topic) => {
+      callback(null, topic);
+    })
+    .catch((err) => {
+      callback(err);
+    });
+  },
+
+  updateTopic(id, updatedTopic, callback){
+    return Topic.findByPk(id)
+    .then((topic) => {
+      if(!topic){
+        return callback("Topic not found");
+      }
+      topic.update(updatedTopic, {
+        fields: Object.keys(updatedTopic)
+      })
+      .then(() => {
+        callback(null, topic);
+      })
+      .catch((err) => {
+        callback(err);
+      });
+    });
   }
+
 }
